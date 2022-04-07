bd913f5a1ff193bcc0f6bda03fc0290d37fabfe1
Beygs <boris.gilles@hotmail.fr>
2022-04-07 14:15:36 +0200

    handle resize

--- src/script.js
+++ src/script.js
@@ -4,6 +4,19 @@ import gsap from "gsap";
 import { SlowMo } from "gsap/all";
 import randomWords from "random-words";
 
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+});
+
 const random = document.querySelector(".random");
 
 // Canvas
